9b0ebc5b4497a5be4e373759311fb27deaabe4c9
Júnior Nunes <denisonprobable13@gmail.com>
2022-05-15 11:53:34 -0300

    global colors style created

---
+++ src/styles/colors.ts
@@ -0,0 +1,14 @@
+const Colors = {
+  carbonDark: '#3b3a3e',
+
+  primary: '#2ecc71',
+  secondary: '#95a5a6',
+  danger: '#e74c3c',
+  light: '#ecf0f1',
+  dark: '#34495e',
+  background: '#ffffff',
+
+  dev: '#f37272',
+};
+
+export default Colors;
